3cded3d339209527245228512abcaf9a1feefdf1
Дубинин Алексей <njn6589@mail.ru>
2023-07-13 07:59:58 +0300

    Добавьте файлы проекта.

---
+++ Task_1/MyConsole.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class MyConsole
+    {
+        private string[] _vowel = { "а", "у", "о", "ы", "и", "э", "я", "ю", "ё", "е" };
+        private StringBuilder _sb = new StringBuilder();
+
+        public void ChangeTheNameOfApp (string userInput)
+        {
+            foreach (var item in _vowel)
+            {
+                if (item == userInput)
+                {                     
+                    _sb.Append(userInput);
+                    Console.Title = _sb.ToString();
+                }
+            }            
+        }
+        public MyConsole()
+        {
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var myConsole = new MyConsole();
+            Console.Title = "";
+            var userInput = "";
+            MenuEnum menuChoice = MenuEnum.None;
+            while (menuChoice != MenuEnum.CloseApp)
+            {
+                Console.Clear();
+                Console.WriteLine("Добрый день!" + Environment.NewLine +
+                "Задача данного приложения обрабатывать вводимые пользователем символы. Вы можете ввести любые символы по одному, однако учитываться будут только буквы. Если вводится гласная буква - ввод принимается. Если согласная - отклоняется. " +
+                Environment.NewLine + "Каждая введенная гласная будет изменять название консольного приложения. Приступим?");
+                Console.WriteLine("1. Ввести символ" + Environment.NewLine +
+                "2. Закончить работу");
+
+                Console.WriteLine("Выберете пункт меню:");
+                menuChoice = (MenuEnum)Convert.ToInt32(Console.ReadLine());
+
+                switch (menuChoice)
+                {
+                    case MenuEnum.ReadTheLine:
+                        Console.Clear();
+                        Console.WriteLine("Введите символ");
+                        userInput = Console.ReadLine().ToLower();
+                        if (userInput.Length > 1)
+                        {
+                            Console.WriteLine("Необходимо ввести один символ русского алфавита. Вы будете возвращены в меню.");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            myConsole.ChangeTheNameOfApp(userInput);
+                        }
+                        break;
+                    case MenuEnum.CloseApp:
+                        break;
+                    default:
+                        Console.WriteLine("Выберете пункт меню");
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+} 
